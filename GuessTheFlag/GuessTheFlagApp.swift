02dd0a1632858641559fc8607d0962ca2fe81897
Jeet Gandhi <jeet.gandhi@zuhlke.com>
2021-03-26 09:37:28 +0800

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jeet Gandhi on 26/3/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
